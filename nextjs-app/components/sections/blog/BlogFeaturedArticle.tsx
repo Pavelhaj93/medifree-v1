ac17169ac9741cbb7cae1401dd385957c5d97dce
Pavelhaj93 <pavelhajduch93@gmail.com>
2025-09-14 23:47:20 +0200

    fix:  build

--- nextjs-app/components/sections/blog/BlogFeaturedArticle.tsx
+++ nextjs-app/components/sections/blog/BlogFeaturedArticle.tsx
@@ -1,7 +1,5 @@
-import { doctors } from "@/app/lib/data/doctors";
 import { Badge } from "@/components/ui/Badge";
 import { Button } from "@/components/ui/Button";
-import type { FirstPostQueryResult } from "@/sanity.types";
 import { sanityFetch } from "@/sanity/lib/live";
 import { firstPostQuery } from "@/sanity/lib/queries";
 import { urlForImage } from "@/sanity/lib/utils";
@@ -15,16 +13,12 @@ export default async function BlogFeaturedArticle() {
     query: firstPostQuery,
   });
 
-  // console.log("ttt first", featuredArticle);
-
-  const post = featuredArticle as FirstPostQueryResult;
-
   return (
     <section className="py-16">
       <div className="container mx-auto px-4">
         <div className="grid md:grid-cols-2 gap-12 items-center">
           <div className="relative h-[400px] rounded-2xl overflow-hidden">
-            <Link href={`/blog/posts/${post?.slug}`}>
+            <Link href={`/blog/posts/${featuredArticle?.slug}`}>
               <Image
                 src="/images/blog/depression.png"
                 alt="Featured article"
@@ -43,31 +37,34 @@ export default async function BlogFeaturedArticle() {
               </Badge>
               <div className="flex items-center text-sm text-gray-500">
                 <CalendarDays className="h-4 w-4 mr-1" />
-                {new Date(post?.date ?? "").toLocaleDateString("cs-CZ", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })}
+                {new Date(featuredArticle?.date ?? "").toLocaleDateString(
+                  "cs-CZ",
+                  {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  }
+                )}
               </div>
               <div className="flex items-center text-sm text-gray-500">
                 <Clock className="h-4 w-4 mr-1" />
-                {post?.readTime} min čtení
+                {featuredArticle?.readTime} min čtení
               </div>
             </div>
-            <Link href={`/blog/posts/${post?.slug}`}>
+            <Link href={`/blog/posts/${featuredArticle?.slug}`}>
               <h2 className="text-3xl hover:underline hover:text-primary transition-colors font-medium mb-4">
-                {post?.title}
+                {featuredArticle?.title}
               </h2>
             </Link>
-            <p className="text-gray-600 mb-6">{post?.description}</p>
+            <p className="text-gray-600 mb-6">{featuredArticle?.description}</p>
             <div className="flex items-center gap-4 mb-6">
               <div className="w-10 h-10 rounded-full overflow-hidden relative">
                 <Image
                   className="object-cover"
                   fill={true}
-                  alt={stegaClean(post?.author?.picture?.alt) || ""}
+                  alt={stegaClean(featuredArticle?.author?.picture?.alt) || ""}
                   src={
-                    urlForImage(post?.author?.picture)
+                    urlForImage(featuredArticle?.author?.picture)
                       ?.height(720)
                       .width(1280)
                       .auto("format")
@@ -76,14 +73,14 @@ export default async function BlogFeaturedArticle() {
                 />
               </div>
               <div>
-                <p className="font-medium">{post?.author?.name}</p>
+                <p className="font-medium">{featuredArticle?.author?.name}</p>
                 <p className="text-sm text-gray-500">
-                  {post?.author?.specialization}
+                  {featuredArticle?.author?.specialization}
                 </p>
               </div>
             </div>
             <Button variant="primary" asChild>
-              <Link href={`/blog/posts/${post?.slug}`}>
+              <Link href={`/blog/posts/${featuredArticle?.slug}`}>
                 Číst článek
                 <ChevronRight className="ml-2 h-4 w-4" />
               </Link>
