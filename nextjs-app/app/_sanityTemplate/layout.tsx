409cf1e190fb75868b2dc52f2252bd8693e5ac6b
Pavelhaj93 <pavelhajduch93@gmail.com>
2025-09-17 09:12:58 +0200

    feat: some try:

--- nextjs-app/app/_sanityTemplate/layout.tsx
+++ nextjs-app/app/_sanityTemplate/layout.tsx
@@ -78,9 +78,7 @@ export default async function RootLayout({
           )}
           {/* The <SanityLive> component is responsible for making all sanityFetch calls in your application live, so should always be rendered. */}
           <SanityLive onError={handleError} />
-          {/* <Header /> */}
           <main className="">{children}</main>
-          {/* <Footer /> */}
         </section>
         <SpeedInsights />
       </body>
